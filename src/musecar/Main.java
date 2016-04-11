12040a16fa31d3e56af2b092804f5965f215abbd
Christopher Wells <cwellsny@nycap.rr.com>
2016-04-11 19:40:03 -0400

    Add car to main method
    Set the main method to create a car object.

--- src/musecar/Main.java
+++ src/musecar/Main.java
@@ -30,7 +30,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        MuseCar museCar = new MuseCar(5000);
     }
 
 }
